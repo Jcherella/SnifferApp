1bc84efbde4c7f630ac5fe40240d072dbffb6171
gajjas <gajjas@rpi.edu>
2020-02-11 16:09:24 -0500

    Created the networkDevice Model

--- lib/models/networkDevice.dart
+++ lib/models/networkDevice.dart
@@ -1,1 +1,13 @@
+class NetworkDevice {
+  var iPAddress;
+  var macAddress;
 
+  NetworkDevice(this.iPAddress, this.macAddress);
+  String getIP() {
+    return this.iPAddress;
+  }
+
+  String getMAC() {
+    return this.macAddress;
+  }
+}
