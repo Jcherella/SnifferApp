e177562ebb93ab83c472f8354ad9c5f4dc76424c
123joshuawu <joshuawu3@gmail.com>
2020-03-26 12:40:37 -0700

    Change models to use InternetAddress class instead of strings for IP addresses

--- lib/models/NetworkInterface.dart
+++ lib/models/NetworkInterface.dart
@@ -1,20 +1,22 @@
-
+import 'dart:io';
 
 /// Represents a network interface on the device
 /// Provides the name (ex. wlan0) and the subnet (IP/mask)
 ///  that the network interface is in
 class NetworkInterface {
-  final String name, ipAddress;
+  final String name;
+  final InternetAddress ip;
   final int subnetMask;
 
-  const NetworkInterface(this.name, this.ipAddress, this.subnetMask);
+  NetworkInterface(this.name, ipAddress, this.subnetMask):
+    this.ip = new InternetAddress(ipAddress);
 
 
   @override
   String toString () {
     return '''
     NetworkInterface ${this.name}
-    Subnet: ${this.ipAddress}/${this.subnetMask}
+    Subnet: ${this.ip.address}/${this.subnetMask}
     ''';
   }
 }
