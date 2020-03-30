b53031d1ad9c1b9e40c593d3afedaade9d90d477
123joshuawu <joshuawu3@gmail.com>
2020-03-30 14:44:10 -0700

    Trial addition of network discovery in loading

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:snifferapp/services/DeviceInfoService.dart';
+import 'package:snifferapp/models/NetworkInterface.dart';
+import 'package:snifferapp/services/PingService.dart';
 
 //Loading Screen: First Screen to see
 class Loading extends StatefulWidget {
@@ -13,8 +16,30 @@ class _LoadingState extends State<Loading> {
   @override
   void initState() {
     super.initState();
-    Timer(
-        Duration(seconds: 5), () => Navigator.pushNamed(context, '/scanPage'));
+//    Timer(
+//        Duration(seconds: 5), () => Navigator.pushNamed(context, '/scanPage'));
+//    DeviceInfoService().loadArpTable().then((a) => Navigator.pushNamed(context, '/scanPage'));
+    DeviceInfoService().loadNetworkInterfaces().then(
+        (success) {
+          return DeviceInfoService().networkInterfaces[0];
+        }
+    ).then(
+        (NetworkInterface networkInterface) {
+          try {
+            return DiscoveryService.discoverNetwork(networkInterface);
+          } catch (e) {
+            return true;
+          }
+        }
+    ).then(
+        (success) {
+          return DeviceInfoService().loadArpTable();
+        }
+    ).then(
+        (success) {
+          return Navigator.pushNamed(context, '/scanPage');
+        }
+    );
   }
 
   //Building the screen
